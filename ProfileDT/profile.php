8cf619bea53766f5f8fe8f9520a012852c9625d9
rodrigopasquim <rodrigopasquim123@Gmail.com>
2025-03-04 10:09:20 -0300

    atl pasta semantica, style p/ styles

--- ProfileDT/profile.php
+++ ProfileDT/profile.php
@@ -36,8 +36,8 @@ $mysqli->close();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style/main-profile.css">
-    <link rel="stylesheet" href="style/header-profile.css">
+    <link rel="stylesheet" href="styles/main-profile.css">
+    <link rel="stylesheet" href="styles/header-profile.css">
     <link rel="shortcut icon" href="https://i.imgur.com/1LdRVjc.png" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
